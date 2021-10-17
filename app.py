e951bd3f8a3d4364b8b3d894aaeb770ddc9a6574
Pedro Faraco <pedrofaraco@gmail.com>
2021-10-17 01:44:53 -0300

    First version

---
+++ app.py
@@ -0,0 +1,16 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: '
+
+blogs = dict() # blog_name : Blog object
+
+def menu():
+    # Show the user the available blogs
+    # Let the user make a choice
+    # Do something with that choice
+    # Eventually quit
+    print_blogs()
+    selection = input(MENU_PROMPT)
+
+def print_blogs():
+    # Print the available blogs
+    for key, blog in blogs.items():
+        print("- {}".format(blog))
---
+++ tests/system/app_test.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest.mock import patch
+import app
+from blog import Blog
+
+class AppTest(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input'):
+-                app.menu()
+                mocked_print_blogs.assert_called()
+
+    def test_print_blogs(self):
+        with patch("builtins.print") as mocked_print:
+            blog = Blog('Test', 'Test Author')
+            app.blogs = {'Test': blog}
+            app.print_blogs()
+            mocked_print.assert_called_with('- Test by Test Author (0 posts)')
+
+
+
