8da14a18dfc9e16d30235970bbedd9b81fad924f
Pedro Faraco <pedrofaraco@gmail.com>
2021-10-17 10:38:53 -0300

    After '64. Taking our patch further'

--- tests/system/app_test.py
+++ tests/system/app_test.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 from unittest.mock import patch
 import app
 from blog import Blog
+from post import Post
+
 
 class AppTest(TestCase):
     def test_menu_prints_prompt(self):
@@ -12,7 +14,7 @@ class AppTest(TestCase):
     def test_menu_calls_print_blogs(self):
         with patch('app.print_blogs') as mocked_print_blogs:
             with patch('builtins.input'):
--                app.menu()
+                app.menu()
                 mocked_print_blogs.assert_called()
 
     def test_print_blogs(self):
@@ -23,4 +25,39 @@ class AppTest(TestCase):
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
 
 
+    def test_ask_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test', 'Test Author')
+            app.ask_create_blog()
+            self.assertIsNotNone(app.blogs.get('Test'))
 
+
+    def test_ask_read_blog(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                app.ask_read_blog()
+                mocked_print_posts.assert_called_with(blog)
+
+
+    def test_print_posts(self):
+        blog = Blog('Test', 'Test Author')
+        blog.create_post('Test Post', 'Test Content')
+        app.blogs = {'Test': blog}
+        with patch('app.print_post') as mocked_print_post:
+            app.print_posts(blog)
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+
+    def test_print_post(self):
+        post = Post('Post title', 'Post content')
+        expected_print = '''
+--- Post title ---
+
+Post content
+
+'''
+        with patch('builtins.print') as mocked_print:
+            app.print_post(post)
+            mocked_print.assert_called_with(expected_print)
